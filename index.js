ed5ac51a12dbf81b11d71c93a74883da955c1c84
itai1989 <itait1989@gmail.com>
2024-10-06 23:15:50 +0300

    הוספת נקודת קצה להוספת נקודת שמירה חדשה (CREATE)

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ let points=[
     {id:0, name:"עמל", description:"בית ספר"   ,time:"7:00"}
 ]
 
+app.post('/AddPoint', (req, res) => {
+    let point={};
+    point.id  = req.body.id;
+    point.name = req.body.name;
+    point.description = req.body.description;
+    point.time = req.body.time;
+    points.push(point);
+
+    res.status(200).json("ok");
+});
 app.get('/pointsList', (req, res) => {
     res.status(200).json(points);
 });
