26baa68033f81afdaa69f77844ae3008d01b08ad
itai1989 <itait1989@gmail.com>
2024-10-06 22:58:31 +0300

    עדכון הגדרות השרת

---
+++ index.js
@@ -0,0 +1,16 @@
+const port = 5050;
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {  
+    console.log(`Now listening on port http://localhost:${port}`);
+});
+
