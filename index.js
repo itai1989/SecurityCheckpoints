dbe1534110e7b98ad486aa3999c0b5ed7d1d84e4
itai1989 <itait1989@gmail.com>
2024-10-06 23:04:41 +0300

    הוספת מערך שיכיל את נקודות השמירה

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ app.listen(port, () => {
     console.log(`Now listening on port http://localhost:${port}`);
 });
 
+//-----------------------------------------------------------------
+let points=[
+    {id:0, name:"עמל", description:"בית ספר"   ,time:"7:00"}
+]
+
+
