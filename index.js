ba88ae519f62cdfbe8c4909fc0c89d04c43f758b
itai1989 <itait1989@gmail.com>
2024-10-13 18:23:22 +0300

    הוספת נקודת שמירה חדשה בצד הלקוח

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 const path = require('path');
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, () => {  
+app.listen(port, () => {
     console.log(`Now listening on port http://localhost:${port}`);
 });
 
 //-----------------------------------------------------------------
 let points=[
-    {id:0, name:"עמל", description:"בית ספר"   ,time:"7:00"}
-]
+    {id: 0 , name:"עמל", description:"בית ספר" ,time:"7:00"}
+];
 
 app.post('/AddPoint', (req, res) => {
     let point={};
-    point.id  = req.body.id;
+    point.id = req.body.id;
     point.name = req.body.name;
     point.description = req.body.description;
     point.time = req.body.time;
@@ -33,26 +33,23 @@ app.get('/pointsList', (req, res) => {
     res.status(200).json(points);
 });
 app.delete('/pointDelete', (req, res) => {
-    let idx=req.query.id;
+    let idx = req.query.id;
     points.splice(idx, 1);
 
     res.status(200).json(points);
 })
 app.post('/pointEdit/:idx', (req, res) => {
-    let idx=req.params.idx;
+    let idx = req.params.idx;
     let point={};
     point.id = req.body.id;
-    point.name=req.body.name;
+    point.name = req.body.name;
     point.description = req.body.description;
     point.time = req.body.time;
 
-    points[idx]=point;
+    points[idx] = point;
 
     res.status(200).json("ok");
 });
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname,"/home.html"));
 });
-
-
-
